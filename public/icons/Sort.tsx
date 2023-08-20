5905268d231a702fa16b04fed38d5c1fae96702f
Zain Sheikh <sheikhzain33@gmail.com>
2023-08-20 16:42:02 +0500

    Allow User To Sort List

---
+++ public/icons/Sort.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Sort = () => {
+  return (
+    <svg
+      width="17"
+      height="17"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />{' '}
+      <path d="M3 9l4-4l4 4m-4 -4v14" /> <path d="M21 15l-4 4l-4-4m4 4v-14" />
+    </svg>
+  );
+};
+
+export default Sort;
--- src/app/Components/Table.tsx
+++ src/app/Components/Table.tsx
@@ -1,16 +1,56 @@
 import { User } from '@/types';
-import React from 'react';
+import React, { useState } from 'react';
+import Sort from '../../../public/icons/Sort';
+
+type Dataset = 'name' | 'companyName' | 'phone' | 'email';
 
 interface TableProps {
   users: User[];
 }
 
+const TABLE_HEADING = [
+  {
+    name: 'Name',
+    dataset: 'name',
+  },
+  {
+    name: 'Email',
+    dataset: 'email',
+  },
+  {
+    name: 'Phone',
+    dataset: 'phone',
+  },
+  {
+    name: 'Company',
+    dataset: 'companyName',
+  },
+  {
+    name: 'LinkedIn',
+  },
+];
+
 const Table = ({ users }: TableProps) => {
+  const [sortedUsers, setSortedUsers] = useState(users);
+
+  const handleSort = (sortedBy: Dataset) => {
+    const sortedList = sortedUsers.sort((a, b) => {
+      if (a[sortedBy] < b[sortedBy]) {
+        return -1;
+      }
+      if (a[sortedBy] > b[sortedBy]) {
+        return 1;
+      }
+      return 0;
+    });
+    setSortedUsers([...sortedList]);
+  };
+
   return (
     <div className="flex flex-col w-full">
       <div className="w-full">
         <div className="inline-block w-full py-2 px-5">
-          {!users?.length ? (
+          {!sortedUsers?.length ? (
             <h1 className="text-center text-lg text-neutral-300">
               No Result Found
             </h1>
@@ -19,25 +59,25 @@ const Table = ({ users }: TableProps) => {
               <table className="min-w-full text-left text-sm font-light">
                 <thead className="border-b bg-neutral-50 font-medium dark:border-neutral-500 dark:text-neutral-800">
                   <tr>
-                    <th scope="col" className="px-6 py-4">
-                      Name
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      Email
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      Phone
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      LinkedIn URL
-                    </th>
-                    <th scope="col" className="px-6 py-4">
-                      Company Name
-                    </th>
+                    {TABLE_HEADING?.map(({ name, dataset }) => (
+                      <th key={name} scope="col" className="px-6 py-4">
+                        <div className="flex flex-row items-center content-center">
+                          <span>{name}</span>
+                          {dataset && (
+                            <button
+                              onClick={() => handleSort(dataset as Dataset)}
+                              className="ml-1 rounded hover:bg-neutral-300 p-1"
+                            >
+                              <Sort />
+                            </button>
+                          )}
+                        </div>
+                      </th>
+                    ))}
                   </tr>
                 </thead>
                 <tbody>
-                  {users?.map((user, index) => (
+                  {sortedUsers?.map((user, index) => (
                     <tr
                       key={index}
                       className="border-b dark:border-neutral-500 hover:bg-neutral-800"
@@ -52,6 +92,9 @@ const Table = ({ users }: TableProps) => {
                         {user?.phone}
                       </td>
                       <td className="whitespace-nowrap px-6 py-4">
+                        {user?.companyName}
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">
                         <a
                           className="rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-indigo-500 transition duration-150 ease-in-out hover:bg-neutral-100 hover:text-indigo-500 focus:text-indigo-500 focus:outline-none focus:ring-0 active:text-indigo-500 dark:hover:bg-neutral-700"
                           href={user?.url}
@@ -59,9 +102,6 @@ const Table = ({ users }: TableProps) => {
                         >
                           Visit
                         </a>
-                      </td>
-                      <td className="whitespace-nowrap px-6 py-4">
-                        {user?.companyName}
                       </td>
                     </tr>
                   ))}
